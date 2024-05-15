dddf5995ebbb499a53bad7523ffb79cf463ee3e0
Kevin Davis <kevindavis@Kevins-MacBook-Pro.local>
2024-05-14 22:32:09 -0500

    fix imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Caudex } from "next/font/google";
-import Navigation from "./Navigation";
+import Navigation from "./navigation";
 import "./globals.css";
 
 const caudex = Caudex({ weight: ["400", "700"], subsets: ["greek"] });
